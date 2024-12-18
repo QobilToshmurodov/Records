678b66857689711829382036df1401d991626960
QobilToshmurodov <qobiltoshmurodov289@gmail.com>
2024-12-18 10:23:24 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Records
+{
+    internal class Program
+    {
+        // 1-Topshiriq:
+        // Employee deb nomlangan positional immutable ko'rinishda (ya'ni positional record uslubida) e'lon qiling.
+        // Uning Name (string), DateJoined (DateTime) va SalaryAmount (decimal)
+        // xossalari bo'lsin.
+
+        // 2-Topshiriq:
+        // Main metodida Employee ga mansub bo'lgan o'zgaruvchi e'lon qiling.
+        // O'zgaruvchining .ToString() metodini chaqirib, natijani konsolga chiqaring.
+
+        static void Main(string[] args)
+        {
+            Employee rec1 = new("Qobil", DateTime.Parse("11.06.2002"), 100000000);
+            Console.WriteLine(rec1.ToString());
+        }
+        public record Employee(string Name, DateTime DateJoined, decimal SalaryAmount);
+    }
+}
